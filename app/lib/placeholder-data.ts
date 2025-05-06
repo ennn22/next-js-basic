3a249fe74d670d984ce2df37473d5b7af0c4c45a
ennn22 <huien122@gmail.com>
2025-05-06 15:16:19 +0800

    fix typo

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -53,7 +53,7 @@ const invoices = [
     customer_id: customers[0].id,
     amount: 15795,
     status: 'pending',
-    date: '2022-12-06',sss
+    date: '2022-12-06',
   },
   {
     customer_id: customers[1].id,
